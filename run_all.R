4d7e36e88ef8d42dd9558335e5f16a55c901af54
Quan Vu <quan.vu@anu.edu.au>
2025-12-01 12:10:55 +0800

    script to run all code

---
+++ run_all.R
@@ -0,0 +1,34 @@
+###### Script to run all the examples in the paper
+
+#########################
+
+####### Simulation in Section 3.3
+
+#### Fit the models for simulated data from deepspat
+source("Examples/sims_1.R")
+#### Results for simulated data from deepspat
+source("Examples/results_sims_1.R")
+
+#### Fit the models for simulated data from cocons
+source("Examples/sims_2.R")
+#### Results for simulated data from cocons
+source("Examples/results_sims_2.R")
+
+
+
+####### Application with mean temperature in Section 4.1
+
+#### Fit the models for mean temperature
+source("Examples/app_model_GP_ST.R")
+#### Results for mean temperature
+source("Examples/app_results_GP_ST.R")
+
+
+
+####### Application with maximum temperature in Section 4.2
+
+#### Fit the models for mean temperature
+source("Examples/app_model_MSP.R")
+#### Results for mean temperature
+source("Examples/app_results_MSP.R")
+
